825dde9336f4e3762956ba446f5e7bbc154ebbc5
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    X00T: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- device.mk
+++ device.mk
@@ -476,7 +476,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-	android.hardware.usb@1.0-service.basic
+	android.hardware.usb@1.0-service.X00T
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk

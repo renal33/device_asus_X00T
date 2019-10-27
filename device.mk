5b9732ee2ff13a61a7a43295a1b49055c217bb84
celtare21 <celtare21@gmail.com>
2019-10-27 13:31:13 +0000

    X00T: powerhint: Import powerhint for sdm660
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Change-Id: Idc906e77ce4f4e4b8a4671b03d103194e33b3adb

--- device.mk
+++ device.mk
@@ -394,7 +394,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.pixel-libperfmgr
 
+# Powerhint
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Public Libraries

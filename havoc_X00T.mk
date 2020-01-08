ef3c8d8590789e827f185e289a46ab256df8cf0d
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-01-08 14:28:02 -0300

    Revert "X00T: Add updatable_apex.mk"
    * X00T doesn't support it as it is a FDE device. This must be defined in the device specific trees of the targets that have FBE as it's a requirement for PSU/Updatable APEX
    
    This reverts commit a066f8b73aa8c156a326cf2b59e657848aa1fcab.
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- havoc_X00T.mk
+++ havoc_X00T.mk
@@ -22,9 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit some common Havoc OS stuff
 $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 

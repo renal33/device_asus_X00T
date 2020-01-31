c9211379b4a7c068a697cba81b6d7219e14602f9
dianlujitao <dianlujitao@lineageos.org>
2020-01-31 20:59:27 +0800

    X00T: Build external audio hal modules
    Change-Id: Ib00ae87142a533285ccfcf5d36876c7a93dd7082

--- device.mk
+++ device.mk
@@ -94,7 +94,10 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
+    liba2dpoffload \
     libaudio-resampler \
+    libhdmiedid \
+    libsndmonitor \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \

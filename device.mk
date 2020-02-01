eae186876cabeb52bd00cf8e5037527ac71c4ac0
seba_3567 <sebastian26481@gmail.com>
2020-02-01 19:35:42 -0300

    X00T: prebuilts: Add Lawnchair
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- device.mk
+++ device.mk
@@ -27,7 +27,19 @@ PRODUCT_COPY_FILES += \
 
 # Prebuilts
 PRODUCT_PACKAGES += \
-    ScreenRecorder
+    ScreenRecorder \
+    LawnConf \
+    Lawnfeed \
+    Lawnchair
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/priv-app/Lawnchair/permissions/privapp-permissions-lawnchair.xml:system/etc/privapp-permissions-lawnchair.xml \
+    $(LOCAL_PATH)/prebuilts/priv-app/Lawnchair/permissions/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.vendor.overlay.theme=com.havoc.overlay.lawnconf
+
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal

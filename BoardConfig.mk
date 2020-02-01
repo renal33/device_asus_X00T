c9ad25d17704618c816d291fe61771701265b031
Saurabh Charde <saurabhchardereal@gmail.com>
2020-02-01 22:07:37 +0530

    X00T: Add back sepolicy from Genkzsz11's tree
    This reverts commit 1dd84c274ca8bd15644f0cd498fe563393608876 and bd316f2ea1c3f7662ddd6befc54376527f923534
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -194,10 +194,8 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2018-06-05
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/minimal_sepolicy
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true

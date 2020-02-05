b5868c28ba89b1f8ca72a845f8f06272ca1f8b7d
Saurabh Charde <saurabhchardereal@gmail.com>
2020-02-05 18:10:09 +0530

    X00T: Go ENFORCING!
    * Revert commit d54e9ca6b0c01ad501cdfb46a8716172c1759ac4 coz we have enforcing muthafukars
    
    P.S: Enforcing sucks though  ¯\_(ツ)_/¯
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- device.mk
+++ device.mk
@@ -399,8 +399,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc \
-    init.safailnet.rc
+    ueventd.qcom.rc
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \

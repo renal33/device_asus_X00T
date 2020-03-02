adc5efbf21607d7719e077f65c29a4d2e3fa789f
Saurabh Charde <saurabhchardereal@gmail.com>
2020-03-02 23:39:28 +0530

    X00T: Don't build liba2dpoffload
    * since it's already disabled

--- device.mk
+++ device.mk
@@ -94,7 +94,6 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
-    liba2dpoffload \
     libaudio-resampler \
     libhdmiedid \
     libsndmonitor \
